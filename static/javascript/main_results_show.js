00f6786ba48ff8dcc66d871c2746a31cd4317dd3
kyZhao-1 <zhaoky24@mails.tsinghua.edu.cn>
2024-10-12 11:37:06 +0800

    Update main_results_show.js

--- static/javascript/main_results_show.js
+++ static/javascript/main_results_show.js
@@ -355,11 +355,11 @@ function createMainResultChart() {
                             let modules = methodModulesMapping[method];  // 获取对应的模块信息
 
                             if (method === 'AgentSquare'){
-                                if currentTask !== 'Avg'{
+                                if (currentTask !== 'Avg'){
                                     modules = agentSquareModules[currentTask];
                                 }
                                 else {
-                                    return ''
+                                    return '';
                                 }
                             }
                 
